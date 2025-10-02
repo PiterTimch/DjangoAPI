721f419dfe120950820c41d744399c78641b39db
Petro <petrotymchuck@gmail.com>
2025-10-02 20:27:32 +0300

    add user table

--- my-react-app/src/App.tsx
+++ my-react-app/src/App.tsx
@@ -1,35 +1,50 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { useEffect, useState } from "react";
+import axios from "axios";
+import type { IUserItem } from "./types/users/IUserItem.ts";
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [users, setUsers] = useState<Array<IUserItem>>([]);
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    const loadList = async () => {
+        try {
+            const res = await axios<IUserItem[]>('http://127.0.0.1:9581/api/users/');
+            setUsers(res.data);
+        }
+        catch (e) {
+            console.error(e)
+        }
+    }
+
+    useEffect(() => {
+        loadList();
+    }, []);
+
+    return (
+        <>
+            <h1>Users</h1>
+            <table className="user-table">
+                <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Імʼя</th>
+                    <th>Прізвище</th>
+                    <th>Email</th>
+                </tr>
+                </thead>
+                <tbody>
+                {users.map((user) => (
+                    <tr key={user.id}>
+                        <td>{user.id}</td>
+                        <td>{user.first_name}</td>
+                        <td>{user.last_name}</td>
+                        <td>{user.email}</td>
+                    </tr>
+                ))}
+                </tbody>
+            </table>
+        </>
+    )
 }
 
 export default App
