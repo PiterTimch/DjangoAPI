1b08ee5d1c35a544a82db484ea9bf54cdbbef1a9
Petro <petrotymchuck@gmail.com>
2025-10-02 08:08:12 +0300

    bad working customUser

--- atbapi/atbapi/settings.py
+++ atbapi/atbapi/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'atbapi'
+    'atbapi',
+    'users'
 ]
 
 MIDDLEWARE = [
@@ -116,6 +117,7 @@ USE_I18N = True
 
 USE_TZ = True
 
+AUTH_USER_MODEL = 'users.CustomUser'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
--- atbapi/atbapi/urls.py
+++ atbapi/atbapi/urls.py
@@ -1,12 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from atbapi.views import UserViewSet
-
-router = DefaultRouter()
-router.register(r'users', UserViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include(router.urls)),
+    path('api/', include('users.urls')),
 ]
